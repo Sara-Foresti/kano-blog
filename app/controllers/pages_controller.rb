fed1ba78ca2a2ce4e32e3e0419dd0252ea5d050e
Sara Foresti <Sara.foresti@hotmail.com>
2018-07-19 12:39:17 +0100

    footer implemented

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,4 +1,5 @@
 class PagesController < ApplicationController
   def home
+    @reader = Reader.new
   end
 end
---
+++ app/controllers/readers_controller.rb
@@ -0,0 +1,18 @@
+class ReadersController < ApplicationController
+  def new
+    @reader = Reader.new
+  end
+
+  def create
+    @reader = Reader.new(reader_params)
+    if @reader.save
+      redirect_to root_path
+    else
+      render :new
+    end
+  end
+
+  def reader_params
+    params.require(:reader).permit(:email, :name, :message)
+  end
+end
